992ef66016a5e5a8f3a938a07117a3bf36566d57
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-18 22:19:14 +0100

    submit: ex03 - norminette ok

---
+++ c12/ex03/ft_list_last.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 22:04:27 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/18 22:18:40 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	if (!begin_list)
+		return (NULL);
+	while (begin_list->next)
+		begin_list = begin_list->next;
+	return (begin_list);
+}
