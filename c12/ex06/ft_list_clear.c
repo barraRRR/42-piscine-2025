4841ef148f2120ac6f7d0318e5a2f8d4f2a1542e
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-19 11:44:43 +0100

    submit: ex06 - norminette ok

---
+++ c12/ex06/ft_list_clear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_clear.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 11:29:06 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/19 11:44:06 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdlib.h>
+
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
+{
+	t_list	*ptr;
+
+	while (begin_list)
+	{
+		ptr = begin_list;
+		begin_list = begin_list->next;
+		free_fct(ptr->data);
+		free(ptr);
+	}
+}
