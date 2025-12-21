01bb048d426ced7d75abb2dbbcca0e3c47103eb5
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-21 14:15:04 +0100

    submit: ex06 - norminette ok

---
+++ c13/ex06/btree_level_count.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_level_count.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 13:40:04 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/21 14:14:39 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+int	btree_level_count(t_btree *root)
+{
+	int		left;
+	int		right;
+
+	if (!root)
+		return (0);
+	left = btree_level_count(root->left);
+	right = btree_level_count(root->right);
+	if (left > right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
